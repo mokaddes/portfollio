7015f6b57d142378be110c18317be74cd3e60dbb
unknown <mr.mokaddes@gmail.com>
2024-08-17 21:16:22 +0600

    up

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -29,7 +29,7 @@
 
     <!-- Contact Us Modal -->
     <div class="modal fade" id="contactModal" tabindex="-1" aria-labelledby="contactModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="contactModalLabel">Hair Me!</h5>
