beb9bd040f1ab999cb28b9f4363fb3c6e67e28ee
unknown <mr.mokaddes@gmail.com>
2024-09-16 12:21:48 +0600

    up

--- resources/views/portfolio/component/header.blade.php
+++ resources/views/portfolio/component/header.blade.php
@@ -1,17 +1,43 @@
 
-<header class="masthead">
+<header class="masthead banner">
     <div class="container">
-        <div class="intro-text">
+        {{--<div class="intro-text">
             <p class="intro-lead-in">Mokaddes Hosain</p>
             <h1 class="intro-heading text-uppercase"><strong>Laravel Developer</strong></h1>
             <a href="{{ asset('assets/cv/mokaddes_hosain.pdf')}}" download="mokaddes">Download CV</a>
-            {{-- <p class="text-muted">1.0.0-beta</p> --}}
-        </div>
-        <div class="py-3">
-            <a class="btn btn-lg btn-outline-secondary js-scroll-trigger" href="#professional">
-                <span class="sr-only">Show more</span>
-                <i class="fa fa-chevron-down"></i>
-            </a>
+        </div>--}}
+        <div class="intro-text">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="mkds">
+                    <img src="{{ asset('images/mkds.jpg') }}" alt="Profile Image" class="profile-image">
+                    <div class="name">Mokaddes Hosain</div>
+                    <div class="profession">Software Developer</div>
+
+                </div>
+                <div class="info">
+                    <div class="contact-info">
+                        <p><i class="fas fa-phone"></i> +8801750899448</p>
+                        <p><i class="fas fa-envelope"></i> mr.mokaddes@gmail.com</p>
+                        <p><i class="fas fa-globe"></i> <a href="https://www.mokaddes.com" target="_blank" >https://www.mokaddes.com</a></p>
+                        <p><i class="fab fa-github"></i> <a href="https://github.com/mokaddes" target="_blank" >https://github.com/mokaddes</a></p>
+                        <p><i class="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/mokaddesru" target="_blank" >https://www.linkedin.com/in/mokaddesru</a></p>
+                    </div>
+                    <div class="text-center">
+                        <a href="{{ asset('assets/cv/mokaddes_hosain.pdf')}}" class="btn btn-light download-btn">
+                            RESUME <i class="fa fa-download"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--<div class="icon">
+                <img src="icons/php.svg" alt="">
+            </div>-->
+            <div class="text-center">
+                <a class="btn btn-lg btn-outline-secondary js-scroll-trigger" href="#professional">
+                    <span class="sr-only">Show more</span>
+                    <i class="fa fa-chevron-down"></i>
+                </a>
+            </div>
         </div>
     </div>
 </header>
--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -11,6 +11,106 @@
         }
 
 
+        .profile-image {
+            border-radius: 50%;
+            width: 180px;
+            height: 180px;
+            object-fit: cover;
+            border: 5px solid #ffc107;
+            z-index: 2;
+        }
+        .banner .mkds{
+            z-index: 2;
+            margin-left: 100px;
+            text-align: center;
+            color: #ffc107;
+        }
+
+        .banner .info {
+            margin-left: 20px;
+            z-index: 2;
+
+        }
+
+        .banner .name {
+            font-size: 2rem;
+            font-weight: bold;
+        }
+
+        .banner .profession {
+            font-size: 1.5rem;
+        }
+
+        .banner .contact-info {
+            font-size: 1.1rem;
+            margin-bottom: 10px;
+            color: #0dcaf0 !important;
+        }
+        .banner .contact-info a{
+            color: #0dcaf0 !important;
+            text-decoration: none;
+        }
+
+        .banner .contact-info p {
+            margin: 5px 0;
+        }
+
+        .banner .contact-info i {
+            margin-right: 10px;
+            color: #ffffff;
+        }
+
+        .banner .social-icons {
+            margin-top: 10px;
+        }
+
+        .banner .social-icons a {
+            color: white;
+            margin-right: 15px;
+            font-size: 1.5rem;
+        }
+
+        .banner .coding-environment h3 {
+            margin-bottom: 20px;
+            position: relative;
+            z-index: 2;
+        }
+
+        .banner .coding-environment p {
+            position: relative;
+            z-index: 2;
+        }
+
+        .overlay-gradient {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background: linear-gradient(to right, rgb(96 96 96 / 53%) 20%, rgb(0 0 0) 100%);
+            z-index: 1;
+        }
+        .banner .icon img{
+            width: 30px;
+            height: 30px;
+            z-index: 2;
+        }
+
+        .download-btn{
+            padding: 0px 15px;
+            border-radius: 10px;
+            font-size: 18px;
+            box-shadow: 0 22px 14px rgba(0, 0, 0, 0.9);
+            color: #000;
+        }
+
+        .download-btn:hover {
+            background: #fdfdfd00;
+            color: #fff;
+        }
+        }
+
+
     </style>
 @endpush
 @section('content')
