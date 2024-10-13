97180a2b3324fd332110703f3de0da8b24144881
unknown <mr.mokaddes@gmail.com>
2024-10-13 18:09:26 +0600

    up

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\VisitorLog;
 use App\Notifications\ContactMailNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
 
 class FrontendController extends Controller
@@ -13,21 +14,25 @@ class FrontendController extends Controller
     public function index()
     {
         $ip = request()->ip();
-        if ($ip !== '103.176.2.79' && $ip !== '127.0.0.1') {
-            $visitData = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-            $visitor = VisitorLog::where('ip_address', $ip)->first();
-            if (!$visitor) {
-                $visitor = new VisitorLog();
+        try {
+            if ($ip !== '103.176.2.79' && $ip !== '127.0.0.1') {
+                $visitData = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+                $visitor = VisitorLog::where('ip_address', $ip)->first();
+                if (!$visitor) {
+                    $visitor = new VisitorLog();
+                }
+                $visitor->ip_address = $ip;
+                $visitor->city = $visitData->city;
+                $visitor->region = $visitData->region;
+                $visitor->country = $visitData->country;
+                $visitor->location = $visitData->loc;
+                $visitor->organization = $visitData->org;
+                $visitor->timezone = $visitData->timezone;
+                $visitor->visit_count = $visitor->visit_count + 1;
+                $visitor->save();
             }
-            $visitor->ip_address = $ip;
-            $visitor->city = $visitData->city;
-            $visitor->region = $visitData->region;
-            $visitor->country = $visitData->country;
-            $visitor->location = $visitData->loc;
-            $visitor->organization = $visitData->org;
-            $visitor->timezone = $visitData->timezone;
-            $visitor->visit_count = $visitor->visit_count + 1;
-            $visitor->save();
+        } catch (\Exception $e) {
+            Log::alert($e->getMessage());
         }
         return view('portfolio.index');
 
