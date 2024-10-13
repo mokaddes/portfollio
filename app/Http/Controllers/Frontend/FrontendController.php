8689aa03a61be69a6cd72196083aec74fa55ef55
unknown <mr.mokaddes@gmail.com>
2024-10-13 18:21:30 +0600

    up

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\VisitorLog;
 use App\Notifications\ContactMailNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
 
